4bae9873981a1fe191dbcde96e70221fd46c9982
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2023-10-19 22:12:14 -0600

    Frases
    Se agregaron algunas frases antes de jugar

--- src/screens/Juego/Español/Español.js
+++ src/screens/Juego/Español/Español.js
@@ -6,11 +6,22 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export function Español() {
   const insets = useSafeAreaInsets();
-
-  const [cont, setCont] = useState(5);
+  const [cont, setCont] = useState(10);
   const [nivel1, setNivel1] = useState(false);
+  const [frases, setFrases] = useState([
+    "Las vocales son las letras especiales del alfabeto: Puedes notar que cuando dices a, e, i, o, y u, tu boca está abierta y no toca ninguna otra parte de tu boca. ¡Eso las hace únicas!",
+    "El sujeto de una oración es la persona o cosa de la que se habla",
+    "El predicado es la parte de la oración que dice algo sobre alguien o algo",
+    "Una tilde es como un sombrero pequeñito que algunas palabras llevan en la cabeza. Ayuda a las palabras a sonar de la manera correcta",
+    "Los sinónimos son palabras que significan casi lo mismo, como si fueran amigos. Un sinónimo de feliz sería contento",
+    "Los antónimos son palabras que son como opuestos, como si fueran contrarios o enemigos. Un antónimo de feliz sería triste",
+  ]);
+  const [random, setRandom] = useState();
 
   useEffect(() => {
+    var x = Math.floor(Math.random() * frases.length);
+    setRandom(x);
+
     var i = cont;
     var inter = setInterval(() => {
       i = i - 1;
@@ -27,9 +38,7 @@ export function Español() {
       style={{
         flex: 1,
         justifyContent: "center",
-
         backgroundColor: "#1f160f",
-
         // Paddings to handle safe area
         paddingTop: insets.top,
         paddingBottom: insets.bottom,
@@ -41,6 +50,29 @@ export function Español() {
         <Nivel1 nivel={1} />
       ) : (
         <View style={{ marginHorizontal: 50, alignItems: "center" }}>
+          <View style={{ marginBottom: 100 }}>
+            <Text
+              style={{
+                color: "red",
+                fontSize: 30,
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
+              ¿Sabias que?
+            </Text>
+            <Text
+              style={{
+                color: "red",
+                fontSize: 20,
+                textAlign: "center",
+                marginTop: 30,
+                letterSpacing: 1,
+              }}
+            >
+              {frases[random]}
+            </Text>
+          </View>
           <Text style={{ color: "red", marginBottom: 30, fontSize: 50 }}>
             {cont}
           </Text>
